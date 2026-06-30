1fcbcdc1662551106f05fdab75b8a8f398dd1564
vinodgoguru <gvinodkumar900@gmail.com>
2026-06-30 14:31:05 +0530

    first commit

--- 40-databases/parameters.tf
+++ 40-databases/parameters.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "sg_id" {
+resource "aws_ssm_parameter" "mysql_root_password" {
   name  = "/${var.project}/${var.environment}/mysql_root_password"
   type  = "SecureString"
   value = var.mysql_root_password

d764d12f2742d33634d9b739fc60a3228a34b06d
vinodgoguru <gvinodkumar900@gmail.com>
2026-07-01 14:51:22 +0530

    first commit

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -4,6 +4,7 @@ locals {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_name = "${var.project}-${var.environment}"
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     common_tags = {
         Project = "${var.project}"
         Environment = "${var.environment}"
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -116,3 +116,79 @@ resource "aws_lb_target_group" "catalogue" {
     unhealthy_threshold = 2
   }
 }
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.common_name}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  force_delete              = false
+
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier       = [local.private_subnet_id]
+
+  target_group_arns = [aws_lb_target_group.catalogue.arn] # Autoscaling launches into specific target group
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+
+  dynamic "tag" {
+    for_each = merge(
+      {
+        Name = "${local.common_name}-catalogue"
+      },
+      local.common_tags
+    )
+    content{
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+
+  # with in 15min autoscaling should be successful to launch instances
+  timeouts {
+    delete = "15m"
+  }
+}
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "${local.common_name}-catalogue"
+  policy_type            = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 75.0
+  }
+}
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = local.backend_alb_listener_arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue.arn
+  }
+
+  condition {
+    host_header {
+      values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
+    }
+  }
+}

a8d007e8e446f1e5d2765d4369add203bd1b8d5a
vinodgoguru <gvinodkumar900@gmail.com>
2026-07-01 14:56:58 +0530

    first commit

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -11,6 +11,10 @@ data "aws_ssm_parameter" "vpc_id" {
     name = "/${var.project}/${var.environment}/vpc_id"
 }
 
+data "aws_ssm_parameter" "backend_alb_listener_arn" {
+    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+}
+
 data "aws_ami" "joindevops" {
   most_recent      = true
   owners           = ["973714476881"]
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -140,7 +140,7 @@ resource "aws_autoscaling_group" "catalogue" {
     preferences {
       min_healthy_percentage = 50
     }
-    triggers = ["launch_template"]
+    #triggers = ["launch_template"]
   }
 
   dynamic "tag" {
